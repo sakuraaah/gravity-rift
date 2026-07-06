097f5d2fa1597121a4456391b5e36f706e8b608b
sakuraaah <niktro30@gmail.com>
2026-07-06 22:28:52 +0300

    feat(player): add spaceship rotation

--- src/game/entities/SpaceShip.tsx
+++ src/game/entities/SpaceShip.tsx
@@ -1,14 +1,22 @@
-import { useCallback } from 'react';
+import { useCallback, useRef } from 'react';
 
-import type { Graphics } from 'pixi.js';
+import { useTick } from '@pixi/react';
+
+import type { Container, Graphics, Ticker } from 'pixi.js';
+
+import { usePlayerControls } from '@/game/systems';
 
 const SPACE_SHIP_SIZE = 44;
 const SPACE_SHIP_POSITION = {
   x: 88,
   y: 88,
 };
+const SPACE_SHIP_ROTATION_SPEED = 0.08;
 
 export function SpaceShip() {
+  const spaceShipRef = useRef<Container>(null);
+  const controlsRef = usePlayerControls();
+
   const drawShip = useCallback((graphics: Graphics) => {
     const halfWidth = SPACE_SHIP_SIZE / 2;
     const halfHeight = SPACE_SHIP_SIZE / 2;
@@ -22,8 +30,28 @@ export function SpaceShip() {
     graphics.fill();
   }, []);
 
+  const updateRotation = useCallback(
+    (ticker: Ticker) => {
+      const spaceShip = spaceShipRef.current;
+
+      if (!spaceShip) {
+        return;
+      }
+
+      const { left, right } = controlsRef.current;
+      const rotationDirection = Number(right) - Number(left);
+
+      spaceShip.rotation +=
+        rotationDirection * SPACE_SHIP_ROTATION_SPEED * ticker.deltaTime;
+    },
+    [controlsRef]
+  );
+
+  useTick(updateRotation);
+
   return (
     <pixiContainer
+      ref={spaceShipRef}
       label="space-ship"
       x={SPACE_SHIP_POSITION.x}
       y={SPACE_SHIP_POSITION.y}
