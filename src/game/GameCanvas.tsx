b544534618f8c2fe8ebd14c9fb393db7a2e93fec
sakuraaah <niktro30@gmail.com>
2026-07-07 20:28:30 +0300

    feat(layout): add fixed canvas dimensions; add scaling

--- src/game/GameCanvas.tsx
+++ src/game/GameCanvas.tsx
@@ -1,18 +1,29 @@
 import { Application } from '@pixi/react';
 
+import { GAME_LAYOUT, GAME_SCALE } from '@/game/constants';
 import { MainScene } from '@/game/scenes';
 
 import './pixi-setup';
 
 export default function GameCanvas() {
   return (
-    <Application
-      antialias={false}
-      background={'#050714'}
-      resizeTo={window}
-      resolution={window.devicePixelRatio}
+    <div
+      className="game-viewport"
+      style={{
+        height: GAME_LAYOUT.Height * GAME_SCALE,
+        width: GAME_LAYOUT.Width * GAME_SCALE,
+      }}
     >
-      <MainScene />
-    </Application>
+      <Application
+        antialias={false}
+        background={'#050714'}
+        className="game-canvas"
+        height={GAME_LAYOUT.Height}
+        resolution={1}
+        width={GAME_LAYOUT.Width}
+      >
+        <MainScene />
+      </Application>
+    </div>
   );
 }
---
+++ src/game/constants/layout.constants.ts
@@ -0,0 +1,6 @@
+export const GAME_LAYOUT = {
+  Height: 270,
+  Width: 480,
+} as const;
+
+export const GAME_SCALE = 3;
