d18a0996f90fc8d4dda38c301f7ed62e209019d0
sakuraaah <niktro30@gmail.com>
2026-08-14 22:09:58 +0300

    fix(black-hole): update black hole assets; fix black hole animation speed

--- src/game/entities/black-hole/blackHole.constants.ts
+++ src/game/entities/black-hole/blackHole.constants.ts
@@ -8,7 +8,7 @@ export const BLACK_HOLE_FRAME_SIZE = BLACK_HOLE_BASE_FRAME_SIZE * GAME_SCALE;
 
 export const BLACK_HOLE_FRAME_RADIUS = BLACK_HOLE_FRAME_SIZE / 2;
 
-export const BLACK_HOLE_ANIMATION_FPS = 10;
+export const BLACK_HOLE_ANIMATION_FPS = 6;
 
 export const BLACK_HOLE_INITIAL_SPAWN = {
   location: { x: 24, y: 24 },
