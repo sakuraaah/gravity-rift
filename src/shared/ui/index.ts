6f051532cde67b103b4d67974cf1e67fd38d519d
sakuraaah <niktro30@gmail.com>
2026-08-20 18:41:11 +0300

    refactor(ui): remove unused component

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,7 +1,6 @@
 export * from './ControlGrid';
 export * from './Grid';
 export * from './Modal';
-export * from './Panel';
 export * from './PixelButton';
 export * from './PixelSlider';
 export * from './PixelSwitch';
