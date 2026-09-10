c286ba45ef85789717e6b8e58e430a65dbd04322
sakuraaah <niktro30@gmail.com>
2026-09-10 19:46:09 +0300

    style(ui): add separate modal controls component

---
+++ src/shared/ui/Modal/ModalControls.ts
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const ModalControls = styled.div({
+  width: 'min(100%, 220px)',
+  margin: '0 auto',
+});
--- src/shared/ui/Modal/index.ts
+++ src/shared/ui/Modal/index.ts
@@ -2,3 +2,4 @@ export * from './Modal';
 export * from './Modal.enums';
 export type * from './Modal.types';
 export * from './useModal';
+export * from './ModalControls';
